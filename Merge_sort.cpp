b20a215d5217873388ec7bcd5b37a5f800d1e35a
Jay Paygude <jaypaygude2@gmail.com>
2025-09-29 09:59:03 +0530

    Create Merge_sort.cpp

---
+++ Merge_sort.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+void merge(int array[], int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    int L[n1], R[n2];
+
+    for (int i = 0; i < n1; i++) L[i] = array[left + i];
+    for (int j = 0; j < n2; j++) R[j] = array[mid + 1 + j];
+
+    int i = 0, j = 0, k = left;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) array[k++] = L[i++];
+        else array[k++] = R[j++];
+    }
+
+    while (i < n1) array[k++] = L[i++];
+    while (j < n2) array[k++] = R[j++];
+}
+
+void merge_sort(int array[], int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+        merge_sort(array, left, mid);
+        merge_sort(array, mid + 1, right);
+        merge(array, left, mid, right);
+    }
+}
+
+int main() {
+    int arr[] = {12, 11, 13, 5, 6, 7};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    merge_sort(arr, 0, size - 1);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
